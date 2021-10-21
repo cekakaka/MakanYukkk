73246ff2ce8ff3eed73fb71c0922c3a42f83ac6d
cekakaka <dwicandra920@gmail.com>
2021-10-21 20:56:30 +0700

    DiscoveryAdapter

--- app/src/main/java/com/dwicandra/makanyukkk/MainActivity.java
+++ app/src/main/java/com/dwicandra/makanyukkk/MainActivity.java
@@ -1,14 +1,49 @@
 package com.dwicandra.makanyukkk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
+
+import com.dwicandra.makanyukkk.Adapter.DiscoveryAdapter;
+import com.dwicandra.makanyukkk.Domain.DiscoveryDomain;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewDiscoveryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewDiscovery();
     }
+
+    private void recyclerViewDiscovery() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewDiscoveryList = findViewById(R.id.discoveryItem);
+        recyclerViewDiscoveryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<DiscoveryDomain> discoveryList = new ArrayList<>();
+        discoveryList.add(new DiscoveryDomain("Vanilla Flaviour"));
+        discoveryList.add(new DiscoveryDomain("Chocolate Flaviour"));
+        discoveryList.add(new DiscoveryDomain("Sayur Asam"));
+        discoveryList.add(new DiscoveryDomain("Tempe Penyet"));
+        discoveryList.add(new DiscoveryDomain("Tempe Goreng"));
+        discoveryList.add(new DiscoveryDomain("Ayam Goyeng"));
+        discoveryList.add(new DiscoveryDomain("Tempe Penyet"));
+
+        adapter = new DiscoveryAdapter(discoveryList);
+        recyclerViewDiscoveryList.setAdapter(adapter);
+
+    }
+
+
 }
