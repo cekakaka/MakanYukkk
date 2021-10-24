62e5a461156213d6d28ee6250a8b8949e3f3f705
cekakaka <dwicandra920@gmail.com>
2021-10-24 14:26:34 +0700

    Food Popular Adapter

--- app/src/main/java/com/dwicandra/makanyukkk/MainActivity.java
+++ app/src/main/java/com/dwicandra/makanyukkk/MainActivity.java
@@ -9,14 +9,16 @@ import android.view.animation.LinearInterpolator;
 import android.widget.LinearLayout;
 
 import com.dwicandra.makanyukkk.Adapter.DiscoveryAdapter;
+import com.dwicandra.makanyukkk.Adapter.PopularAdapter;
 import com.dwicandra.makanyukkk.Domain.DiscoveryDomain;
+import com.dwicandra.makanyukkk.Domain.FoodDomain;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView.Adapter adapter;
-    private RecyclerView recyclerViewDiscoveryList;
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewDiscoveryList, recyclerViewPopularList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerViewDiscovery();
+        recyclerViewPopularList();
+    }
+
+    private void recyclerViewPopularList() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewPopularList = findViewById(R.id.popularItem);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodList = new ArrayList<>();
+        foodList.add(new FoodDomain("Potato Chips","food1","",5.0));
+        foodList.add(new FoodDomain("Omlette","food2","",5.0));
+
+        adapter2 = new PopularAdapter(foodList);
+        recyclerViewPopularList.setAdapter(adapter2);
     }
 
     private void recyclerViewDiscovery() {
